ebeb62378cddb6c807c659f7868f390e5c06e043
snowythevulpix <vulpix773@gmail.com>
2023-08-11 20:46:10 +0100

    dark mode by defult bc you need eyes to watch

--- nomorebs.py
+++ nomorebs.py
@@ -1,30 +1,22 @@
 import os
+import fileinput
 
-def replace_directory_in_url(url, search, replace):
-    # Split the URL into parts
-    parts = url.split('/')
+# Get the current directory where the script is located
+current_directory = os.path.dirname(os.path.abspath(__file__))
 
-    # Replace the search string with the replace string
-    modified_parts = [replace if part == search else part for part in parts]
-
-    # Reconstruct the modified URL
-    modified_url = '/'.join(modified_parts)
-
-    return modified_url
-
-# List of directory URLs
-directory_urls = [
-    "https://consumet-api-production-0afb.up.railway.app/dir1/",
-    "https://consumet-api-production-0afb.up.railway.app/dir2/subdir/",
-    # Add more URLs as needed
-]
-
-search_string = "https://consumet-api-production-0afb.up.railway.app"
-replace_string = "https://c.delusionz.xyz"
-
-# Replace directories in each URL
-modified_urls = [replace_directory_in_url(url, search_string, replace_string) for url in directory_urls]
-
-# Print the modified URLs
-for modified_url in modified_urls:
-    print(modified_url)
+# Iterate through all .html files in the current directory
+for root, dirs, files in os.walk(current_directory):
+    for file in files:
+        if file.endswith(".html"):
+            file_path = os.path.join(root, file)
+            
+            # Perform the modification using fileinput module
+            with fileinput.FileInput(file_path, inplace=True) as f:
+                for line in f:
+                    # Check if the line contains a <body> tag
+                    if '<body' in line:
+                        # Add dark-mode class to the body tag
+                        modified_line = line.replace('<body', '<body class="dark-mode"')
+                        print(modified_line, end='')
+                    else:
+                        print(line, end='')

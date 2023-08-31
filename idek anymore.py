b8098596932c889812141ee080678feaee98ab30
snowythevulpix <vulpix773@gmail.com>
2023-09-01 00:15:14 +0100

    new design for all eps

--- idek anymore.py
+++ idek anymore.py
@@ -1,30 +1,49 @@
 import os
 from bs4 import BeautifulSoup
 
-def remove_download_button(file_path):
-    with open(file_path, 'r') as file:
-        content = file.read()
+# HTML template for the top bar
+top_bar_template = """
+<div class="top-bar">
+    <a href="https://snowythevulpix.github.io/redirect" title="Go to Home">
+        <img class="logo" src="file:///C:/Users/Hayle/OneDrive/Desktop/psyduckanime%20v2/psyduckanime-main/Psyduck.png" alt="Logo"/>
+    </a>
+    PsyduckAnime - watch pokemon subbed and dubbed for free
+</div>
+"""
 
-    soup = BeautifulSoup(content, 'html.parser')
-
-    # Remove the download button if it exists
-    download_button = soup.find("button", onclick="redirectToDownload()")
-    if download_button:
-        download_button.extract()
-
-        # Save the modified content back to the file
-        with open(file_path, 'w') as file:
-            file.write(str(soup))
-
-if __name__ == "__main__":
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-
-    # List of files to exclude the download button from
-    excluded_files = ["home.html", "index.html"]
-
-    for root, _, files in os.walk(current_directory):
-        for file in files:
-            if file.endswith('.html') and file in excluded_files:
-                file_path = os.path.join(root, file)
-                remove_download_button(file_path)
-                print(f"Removed download button from: {file_path}")
+# Traverse the directory tree and process each HTML file
+for root, _, files in os.walk("."):
+    for filename in files:
+        if filename.endswith(".html"):
+            filepath = os.path.join(root, filename)
+            
+            # Read the original HTML file
+            with open(filepath, "r", encoding="utf-8") as file:
+                original_html = file.read()
+            
+            # Parse the HTML using Beautiful Soup
+            soup = BeautifulSoup(original_html, "html.parser")
+            
+            # Remove any existing top bars
+            for top_bar in soup.find_all(class_="top-bar"):
+                top_bar.extract()
+            
+            # Create the top bar tag
+            top_bar_tag = soup.new_tag("div")
+            top_bar_tag["class"] = "top-bar"
+            a_tag = soup.new_tag("a", href="https://snowythevulpix.github.io/redirect", title="Go to Home")
+            img_tag = soup.new_tag("img", class_="logo", src="file:///C:/Users/Hayle/OneDrive/Desktop/psyduckanime%20v2/psyduckanime-main/Psyduck.png", alt="Logo")
+            img_tag["style"] = "width: 40px; height: 40px; padding: 5px; cursor: pointer;"  # Apply logo size and padding
+            a_tag.append(img_tag)
+            top_bar_tag.append(a_tag)
+            top_bar_tag.append("PsyduckAnime - watch pokemon subbed and dubbed for free")
+            
+            # Insert the top bar template at the beginning of the body
+            body_tag = soup.body
+            body_tag.insert(0, top_bar_tag)
+            
+            # Write the modified HTML back to the file
+            with open(filepath, "w", encoding="utf-8") as file:
+                file.write(str(soup))
+            
+            print(f"Top bar added to {filepath}")

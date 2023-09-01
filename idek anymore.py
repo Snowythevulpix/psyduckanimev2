42a53092656c0276a308261c3bb63b6cf0ecf769
snowythevulpix <vulpix773@gmail.com>
2023-09-01 12:31:08 +0100

    removed out of place text from homepage

--- idek anymore.py
+++ idek anymore.py
@@ -1,28 +1,29 @@
 import os
 from bs4 import BeautifulSoup
 
+# New logo URL
+new_logo_url = "https://vulpix500.gay/Psyduck.png"
+
 # Traverse the directory tree and process each HTML file
 for root, _, files in os.walk("."):
     for filename in files:
-        if filename == "home.html":
+        if filename.endswith(".html"):
             filepath = os.path.join(root, filename)
-
+            
             # Read the original HTML file
             with open(filepath, "r", encoding="utf-8") as file:
                 original_html = file.read()
-
+            
             # Parse the HTML using Beautiful Soup
             soup = BeautifulSoup(original_html, "html.parser")
-
-            # Find and remove the specific text
-            text_to_remove = "PsyduckAnime - watch pokemon subbed and dubbed for free"
-            text_elements = soup.find_all(text=True)
-            for element in text_elements:
-                if text_to_remove in element:
-                    element.replace_with("")
-
+            
+            # Find and update the logo img tag
+            logo_img = soup.find("img", class_="logo")
+            if logo_img:
+                logo_img["src"] = new_logo_url
+            
             # Write the modified HTML back to the file
             with open(filepath, "w", encoding="utf-8") as file:
                 file.write(str(soup))
-
-            print(f"Text removed from {filepath}")
+            
+            print(f"Logo updated in {filepath}")

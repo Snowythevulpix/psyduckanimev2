41dca71f2c91178e1804de9889866d16d4e36c06
snowythevulpix <vulpix773@gmail.com>
2023-09-01 11:18:33 +0100

    fixed logo?

--- idek anymore.py
+++ idek anymore.py
@@ -1,20 +1,13 @@
 import os
 from bs4 import BeautifulSoup
 
-# HTML template for the top bar
-top_bar_template = """
-<div class="top-bar">
-    <a href="https://snowythevulpix.github.io/redirect" title="Go to Home">
-        <img class="logo" src="file:///C:/Users/Hayle/OneDrive/Desktop/psyduckanime%20v2/psyduckanime-main/Psyduck.png" alt="Logo"/>
-    </a>
-    PsyduckAnime - watch pokemon subbed and dubbed for free
-</div>
-"""
+# New logo URL
+new_logo_url = "https://vulpix500.gay/Psyduck.png"
 
 # Traverse the directory tree and process each HTML file
 for root, _, files in os.walk("."):
     for filename in files:
-        if filename.endswith(".html"):
+        if filename.startswith("ep") and filename.endswith(".html"):
             filepath = os.path.join(root, filename)
             
             # Read the original HTML file
@@ -24,26 +17,13 @@ for root, _, files in os.walk("."):
             # Parse the HTML using Beautiful Soup
             soup = BeautifulSoup(original_html, "html.parser")
             
-            # Remove any existing top bars
-            for top_bar in soup.find_all(class_="top-bar"):
-                top_bar.extract()
-            
-            # Create the top bar tag
-            top_bar_tag = soup.new_tag("div")
-            top_bar_tag["class"] = "top-bar"
-            a_tag = soup.new_tag("a", href="https://snowythevulpix.github.io/redirect", title="Go to Home")
-            img_tag = soup.new_tag("img", class_="logo", src="file:///C:/Users/Hayle/OneDrive/Desktop/psyduckanime%20v2/psyduckanime-main/Psyduck.png", alt="Logo")
-            img_tag["style"] = "width: 40px; height: 40px; padding: 5px; cursor: pointer;"  # Apply logo size and padding
-            a_tag.append(img_tag)
-            top_bar_tag.append(a_tag)
-            top_bar_tag.append("PsyduckAnime - watch pokemon subbed and dubbed for free")
-            
-            # Insert the top bar template at the beginning of the body
-            body_tag = soup.body
-            body_tag.insert(0, top_bar_tag)
+            # Find and update the logo img tag
+            logo_img = soup.find("img", class_="logo")
+            if logo_img:
+                logo_img["src"] = new_logo_url
             
             # Write the modified HTML back to the file
             with open(filepath, "w", encoding="utf-8") as file:
                 file.write(str(soup))
             
-            print(f"Top bar added to {filepath}")
+            print(f"Logo updated in {filepath}")

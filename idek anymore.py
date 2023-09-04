fe1dc893d62d4eccadb9a4af21b55b1bdf8eeb33
snowythevulpix <vulpix773@gmail.com>
2023-09-04 17:35:36 +0100

    fixed stuff

--- idek anymore.py
+++ idek anymore.py
@@ -1,29 +1,45 @@
 import os
 from bs4 import BeautifulSoup
 
-def remove_next_episode_button(file_path):
-    with open(file_path, 'r') as file:
-        content = file.read()
-        soup = BeautifulSoup(content, 'html.parser')
+# Define the HTML code you want to insert (excluding line 43)
+inserted_code = """
+    <!-- Your modified HTML code here -->
+"""
 
-        # Find and remove the button with the 'onclick' attribute set to 'nextEpisode()'
-        button_tags = soup.find_all('button', {'onclick': 'nextEpisode()'})
-        for button_tag in button_tags:
-            button_tag.extract()  # Remove the button element
+# Get the current directory where the script is located
+directory_path = os.path.dirname(os.path.abspath(__file__))
 
-    with open(file_path, 'w') as file:
-        file.write(str(soup))
+# List of files to ignore
+ignore_files = ['index.html', 'discord.html', 'home.html', 'test.html']
 
-def remove_next_episode_button_from_specific_files(directory):
-    specific_files = ['home.html', 'discord.html', 'test.html', 'index.html']
+# Function to modify an HTML file
+def modify_html_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
 
-    for root, _, files in os.walk(directory):
-        for file_name in files:
-            if file_name in specific_files and file_name.endswith('.html'):
-                file_path = os.path.join(root, file_name)
-                remove_next_episode_button(file_path)
-                print(f"Removed 'Next Episode' button from {file_path}")
+    # Check if the file should be ignored
+    if os.path.basename(file_path) not in ignore_files:
+        # Find line 43 and keep it as is
+        for i, line in enumerate(lines):
+            if i == 42:  # Line 43 (0-based index)
+                continue
+            # Insert the code at the desired location
+            if '</head>' in line:
+                lines.insert(i, inserted_code)
+                break
 
-if __name__ == "__main__":
-    target_directory = "."  # Change this to the directory containing your HTML files
-    remove_next_episode_button_from_specific_files(target_directory)
+    # Write the modified HTML back to the file
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.writelines(lines)
+
+# Function to traverse the directory and modify HTML files
+def modify_files_in_directory(directory_path):
+    for root, dirs, files in os.walk(directory_path):
+        for filename in files:
+            if filename.endswith('.html'):
+                file_path = os.path.join(root, filename)
+                print(f'Modifying file: {file_path}')
+                modify_html_file(file_path)
+
+# Call the function to start modifying HTML files
+modify_files_in_directory(directory_path)

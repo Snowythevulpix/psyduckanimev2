cf3eba521fb0021840fa0370af064792746b2ee1
Snowythevulpix <118809222+Snowythevulpix@users.noreply.github.com>
2023-10-19 19:51:31 +0100

    Revert "changed api bc old one is no longer mainstained"
    This reverts commit 0010867df5ff779a158ac2a746ba976d9c1797a9.

--- idek anymore.py
+++ idek anymore.py
@@ -1,27 +1,36 @@
 import os
+import re
 
-# Directory path to start the search and replace
-start_directory = os.path.dirname(__file__)  # Use the directory of the script
+# Get user input for the episode ID and directory to be edited
+episode_id = input("Enter the episode ID: ")
+directory = input("Enter the directory path to be edited: ")
 
-# Replace this string
-search_string = "https://c.delusionz.xyz"
+# Read the content from ep1.html
+with open(r'C:\Users\Hayle\OneDrive\Desktop\psyduckanime v2\psyduckanime-main\sub\sm\ep1.html', 'r', encoding='utf-8') as ep1_file:
+    ep1_content = ep1_file.read()
 
-# Replace with this string
-replace_string = "https://psyduckanime.xyz"
-
-# Function to search and print changes without modifying the file
-def search_and_print_changes(file_path):
-    with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
-        for line in file:
-            modified_line = line.replace(search_string, replace_string)
-            if modified_line != line:
-                print(f"Modifying {file_path}: {line.strip()} -> {modified_line.strip()}")
-    
-# Recursively traverse the directory and its subdirectories
-for root, dirs, files in os.walk(start_directory):
+for root, dirs, files in os.walk(directory):
     for file in files:
-        file_path = os.path.join(root, file)
-        if os.path.isfile(file_path):
-            search_and_print_changes(file_path)
+        if file.endswith(".html") and file != "home.html":
+            file_path = os.path.join(root, file)
+            episode_match = re.search(r'ep(\d+)\.html', file)
+            if episode_match:
+                episode_number = episode_match.group(1)
+                # Clear the content of the file
+                with open(file_path, 'w', encoding='utf-8') as clear_file:
+                    pass
 
-print("Search and print changes complete.")
+                # Write the content from ep1.html
+                with open(file_path, 'a', encoding='utf-8') as modified_file:
+                    modified_file.write(ep1_content)
+
+                # Update the episodeId variable
+                with open(file_path, 'r', encoding='utf-8') as original_file:
+                    content = original_file.read()
+
+                modified_content = re.sub(r'var episodeId = "pokemon-sun-moon-episode-\d+";', f'var episodeId = "{episode_id}-{episode_number}";', content)
+
+                with open(file_path, 'w', encoding='utf-8') as modified_file:
+                    modified_file.write(modified_content)
+
+                print(f"Modified: {file_path}")

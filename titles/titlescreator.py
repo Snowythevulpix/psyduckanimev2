11f800928c08d7e85bad1d83fd98b5eace187af2
Snowythevulpix <118809222+Snowythevulpix@users.noreply.github.com>
2023-11-26 17:22:30 +0000

    added pokemon XYZ sub and dub, edited sm titles

--- titles/titlescreator.py
+++ titles/titlescreator.py
@@ -1,33 +1,31 @@
 import json
 
-# File paths
-input_file = r'C:\Users\Hayle\psyduckanimev2\titles\titles.txt'
-output_file = r'C:\Users\Hayle\psyduckanimev2\titles\sm.json'
+def create_json_from_file(input_file, output_file):
+    with open(input_file, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
 
-# Read data from smtitles.txt and process
-episode_data = {}
-with open(input_file, 'r', encoding='latin-1', errors='ignore') as file:  # Try reading with latin-1 encoding
-    for line in file:
-        parts = line.strip().split('|')
-        episode_id = parts[1].replace('SM', '')
-        episode_data[episode_id] = {
-            "ID": int(episode_id),
-            "EnglishTitle": parts[2],
-            "JapaneseTitle": parts[3],
-            "JapaneseTransliteration": parts[4],
-            "AirDate": {
-                "International": parts[5],
-                "Japanese": parts[6]
-            }
-        }
+    data = []
+    start_processing = False
+    for line in lines:
+        if '{{Episodelistbody' in line:
+            start_processing = True
+            continue
 
-# Check if data was read correctly
-print("Number of episodes processed:", len(episode_data))
+        if start_processing:
+            episode_data = line.strip().split('|')
+            if len(episode_data) >= 5:
+                episode_info = {
+                    "Episode Number": episode_data[0],
+                    "English Title": episode_data[1],
+                    "Japanese Title": episode_data[2],
+                    "Air Date (Japan)": episode_data[3],
+                    "Air Date (US)": episode_data[4]
+                }
+                data.append(episode_info)
 
-# Write formatted data to smtitles.json with ensure_ascii=False
-try:
     with open(output_file, 'w', encoding='utf-8') as json_file:
-        json.dump(episode_data, json_file, indent=2, ensure_ascii=False)
-    print("Data successfully written to smtitles.json")
-except Exception as e:
-    print("Error writing data:", e)
+        json.dump(data, json_file, indent=2, ensure_ascii=False)
+
+# Replace 'titles.txt' with your file's path and name
+# Replace 'sm.json' with the desired output JSON file's path and name
+create_json_from_file(r'C:\Users\Hayle\psyduckanimev2\titles\titles.txt', r'C:\Users\Hayle\psyduckanimev2\titles\sm.json')

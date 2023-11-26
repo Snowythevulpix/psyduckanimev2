18e69f3bc8c9be71546da67ca0cc1b0da9c51c45
Snowythevulpix <118809222+Snowythevulpix@users.noreply.github.com>
2023-11-26 13:14:58 +0000

    working on sm titles

---
+++ titles/titlescreator.py
@@ -0,0 +1,33 @@
+import json
+
+# File paths
+input_file = r'C:\Users\Hayle\psyduckanimev2\titles\smtitles.txt'
+output_file = r'C:\Users\Hayle\psyduckanimev2\titles\smtitles.json'
+
+# Read data from smtitles.txt and process
+episode_data = {}
+with open(input_file, 'r', encoding='latin-1', errors='ignore') as file:  # Try reading with latin-1 encoding
+    for line in file:
+        parts = line.strip().split('|')
+        episode_id = parts[1].replace('SM', '')
+        episode_data[episode_id] = {
+            "ID": int(episode_id),
+            "EnglishTitle": parts[2],
+            "JapaneseTitle": parts[3],
+            "JapaneseTransliteration": parts[4],
+            "AirDate": {
+                "International": parts[5],
+                "Japanese": parts[6]
+            }
+        }
+
+# Check if data was read correctly
+print("Number of episodes processed:", len(episode_data))
+
+# Write formatted data to smtitles.json with error handling
+try:
+    with open(output_file, 'w') as json_file:
+        json.dump(episode_data, json_file, indent=2)
+    print("Data successfully written to smtitles.json")
+except Exception as e:
+    print("Error writing data:", e)
